5ee8c74ca66e4b37c2f6d0eb809862049d511c5d
prince31305 <prince31305@gmail.com>
2026-03-15 15:51:53 +0000

    conversion operation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "99"
+
+// console.log(typeof score );
+// console.log (typeof(score));
+
+  let convertInNumber = Number(score)
+
+//  console.log(typeof convertInNumber);
+//   console.log(convertInNumber);
+
+  /*
+  "33" => 33
+  "33ree" => NaN
+  true => 1
+  false => 0
+  
+  */
+ let isLoggedIn =0
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ //console.log(booleanIsLoggedIn)
+
+ // 1 => true    // 0=> false
+ // "" => false    // "prince" => true
+
+ let someNumber = 68
+
+ let stringNumber = String(someNumber)
+
+ console.log(stringNumber)
+ console.log( typeof stringNumber)
