bc1aac140b230a476fd0c17737093e1157d6287a
prince31305 <prince31305@gmail.com>
2026-03-15 17:08:40 +0000

    some changes

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -28,5 +28,42 @@ let score = "99"
 
  let stringNumber = String(someNumber)
 
- console.log(stringNumber)
- console.log( typeof stringNumber)
+ //console.log(stringNumber)
+ //console.log( typeof stringNumber)
+
+
+ //************************************************ operation ***************************************
+
+ let value =90
+ let negValue = -value
+//  console.log(negValue)
+
+//  console.log(2+2)
+//  console.log (2*2)
+//  console.log (2-2)
+//  console.log(2%2)
+//  console.log(2/2)
+
+
+
+let str1 = "1"
+let str2 = " hello"
+let str3 = str1 + str2
+//console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "3")
+// console.log("3" + 2 +1)
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+
+let myName = 1
+++myName;
+console.log(myName)
+
