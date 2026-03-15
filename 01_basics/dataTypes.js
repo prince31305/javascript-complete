bd18f7e884d5fd9abd14a918c5e8a23c29839853
prince31305 <prince31305@gmail.com>
2026-03-15 13:10:04 +0000

    dataTypes and variable

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"  // treat all js code as newer version
+
+// alert (3+3)  // we are using node js not browser
+
+let states;
+
+// console.log(typeof states )
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined )   // undifined
+console.log(typeof null )         //object
